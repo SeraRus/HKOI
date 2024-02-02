90ef5cbd782f51890ed417257d3e86b21d3aa812
SeraRus <51085940+SeraRus@users.noreply.github.com>
2024-02-03 00:57:19 +0800

    Create D301.py

---
+++ Python/D301.py
@@ -0,0 +1,9 @@
+l1=input()
+l2=input()
+l3=input()
+l4=input()
+l5=input()
+print(f"""My name is {l1}.
+I study at {l5} Secondary School.
+I go to school by {l4} with my classmate {l2}.
+We both like {l3}.""")
