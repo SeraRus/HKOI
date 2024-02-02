9f142282c345ae87b06d64f8f7b222914f1a9ae7
SeraRus <51085940+SeraRus@users.noreply.github.com>
2024-02-03 01:13:47 +0800

    Create D804.py

---
+++ Python/D804.py
@@ -0,0 +1,19 @@
+input()
+First = list(map(int, input().split(" ")))
+Second = list(map(int, input().split(" ")))
+Temp1 = []
+
+
+while len(First) != 0 and len(Second) != 0:
+    if First[0] < Second[0]:
+        Temp1.append(First.pop(0))
+    else:
+        Temp1.append(Second.pop(0))
+
+if len(First) != 0:
+    Temp1 += First
+else:
+    Temp1 += Second
+
+for i in Temp1:
+    print(i, end=" ")
