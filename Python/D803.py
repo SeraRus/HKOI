0fb338c9b0c971d3c7c946cd43b1a7a21a901b5f
SeraRus <51085940+SeraRus@users.noreply.github.com>
2024-02-05 23:10:43 +0800

    Create D803.py

---
+++ Python/D803.py
@@ -0,0 +1,18 @@
+input()
+bol = 0
+nums = list(map(int, input().split(" ")))
+print(nums[0])
+sortedList = [nums.pop(0)]
+for i in range(len(nums)):
+    temp = nums.pop(0)
+    for u in range(len(sortedList)):
+        if temp <= sortedList[u]:
+            sortedList.insert(u, temp)
+            bol += 1
+            break
+    if bol == 0:
+        sortedList.append(temp)
+    for u in sortedList:
+        print(u, end=' ')
+    print()
+    bol = 0
