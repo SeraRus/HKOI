450bef08d9229270776b73459c645f1d2518b0da
SeraRus <51085940+SeraRus@users.noreply.github.com>
2024-02-06 15:55:35 +0800

    Create Q202.sql

---
+++ SQL/Q202.sql
@@ -0,0 +1,4 @@
+SELECT * FROM book WHERE isbn = 9781974715466;
+SELECT title FROM book WHERE genre = 'Technology';
+SELECT title, num_pages FROM book WHERE author = 'Donald Knuth';
+SELECT DISTINCT author FROM book
