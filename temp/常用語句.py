ac9a8ceb47b18cc13f743e6244496571398c7740
SeraRus <51085940+SeraRus@users.noreply.github.com>
2024-03-10 21:56:24 +0800

    Update 常用語句.py

--- temp/常用語句.py
+++ temp/常用語句.py
@@ -1,4 +1,35 @@
-N = int(input().strip())
-fare_matrix = [list(map(float, input().strip().split())) for _ in range(N)]
+#單行輸入轉換列表
+list = list(map(int, input().split()))
 
+#多行輸入轉換二維列表
+matrix = [list(map(int, input().split())) for _ in range(int(input()))]
 
+#歸併排序
+def merge_sort(alist):
+    if len(alist) > 1:
+        mid = len(alist) // 2
+        left_half = alist[:mid]
+        right_half = alist[mid:]
+
+        merge_sort(left_half)
+        merge_sort(right_half)
+
+        i = j = k = 0
+        while i < len(left_half) and j < len(right_half):
+            if left_half[i] < right_half[j]:
+                alist[k] = left_half[i]
+                i += 1
+            else:
+                alist[k] = right_half[j]
+                j += 1
+            k += 1
+
+        while i < len(left_half):
+            alist[k] = left_half[i]
+            i += 1
+            k += 1
+
+        while j < len(right_half):
+            alist[k] = right_half[j]
+            j += 1
+            k += 1
