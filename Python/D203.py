4c4dfcec33f6580dc03a8331c52f3a9994a06a42
SeraRus <51085940+SeraRus@users.noreply.github.com>
2024-03-19 22:18:32 +0800

    Update D203.py

--- Python/D203.py
+++ Python/D203.py
@@ -1,22 +1,12 @@
-s = int(input())
-n = 1
-t = 0
-t2 = 0
-while t != 100:
-  if t2 == 10:
-    print("")
-    t2 = 0
-  else:
-    pass
-  t2=t2+1
-  if n % s == 0:
-    print("Clap ",end = "")
-  else:
-    n1=str(n)
-    s1=str(s)
-    if s1 in n1:
-      print("Clap ",end = "")
+num = input()
+for ptn in range(1,101):
+
+    if num in str(ptn):
+        print("Clap", end=" ")
+    elif ptn % int(num) == 0:
+        print("Clap", end=" ")
     else:
-      print(f"{n} ",end = "")
-  t=t+1
-  n=n+1
+        print(ptn, end=" ")
+
+    if ptn % 10 == 0:
+        print()
